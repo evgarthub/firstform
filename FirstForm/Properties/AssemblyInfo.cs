c11386042bdeebca22e07cd9057b9e594f629588
Yevhen Kifiak <ykifiak@sdl.com>
2017-10-03 18:59:20 +0300

    Add project files.

---
+++ FirstForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FirstForm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FirstForm")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("34a08e33-941f-4b41-8921-1cb25f3b0ecf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FirstForm/RequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+
+namespace FirstForm
+{
+    public class RequestHandler : IHttpHandler
+    {
+
+        public bool IsReusable { get { return false; } }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest Request = context.Request;
+            HttpResponse Response = context.Response;
+
+            string pageName = Request.PhysicalApplicationPath + Request.CurrentExecutionFilePath;
+
+            NameValueCollection formValue;
+            if (Request.Form != null)
+            {
+                formValue = Request.Form;
+                var fileUpload = Request.Form["file"];
+            }
+
+            Response.TransmitFile(pageName);
+
+
+        }
+    }
+}
